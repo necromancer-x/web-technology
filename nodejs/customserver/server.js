633535d21ac01341c1e5fa1aa7855c3c1b7f7333
necromancer-x <jayvrat.expertiz@gmail.com>
2026-02-12 12:17:57 +0530

    add nodejs folder

---
+++ nodejs/customserver/server.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let fileName = req.url === "/" ? "index.html" : req.url;
+
+  if (!path.extname(fileName)) {
+    fileName += ".html";
+  }
+
+  const filePath = path.join(__dirname, fileName);
+
+  const extName = path.extname(filePath).toLowerCase();
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404: File Not Found");
+      } else {
+        res.writeHead(500);
+        res.end("Server Error");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
